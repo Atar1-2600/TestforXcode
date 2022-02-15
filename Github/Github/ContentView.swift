c2d1e1121e7307e320da4a35a319bcfafcd7fdcb
Lucas B <lsbarkmeier@gmail.com>
2022-02-15 14:10:04 -0600

    PogTest
    ...

---
+++ Github/Github/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Github
+//
+//  Created by 64008786 on 2/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Github/Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by 64008786 on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
